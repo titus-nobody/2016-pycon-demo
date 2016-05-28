b8f57eb7beef3fd603da83820fb13419733b3b4c
C. Titus Brown <titus@idyll.org>
2016-05-28 09:44:50 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='projy-mc-proj',
+      version='0.1',
+      description='test stuff for pycon tutorial',
+      py_modules=['wordcount_lib'],
+      scripts=['wordcount'],
+      setup_requires=[
+          'pytest-runner',
+      ],
+      tests_require=[
+          'pytest',
+      ],
+)
